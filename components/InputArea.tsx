92c2aa48c6a9b9a876cd1191296a3d020ba48f99
CharlEdwards <504sloan@gmail.com>
2025-12-30 21:47:17 -0500

    Add files via upload

--- components/InputArea.tsx
+++ components/InputArea.tsx
@@ -106,7 +106,7 @@ const InputArea: React.FC<InputAreaProps> = ({ onSendMessage, isLoading }) => {
         </div>
       </form>
       <div className="h-6"></div>
-      <p className="text-center text-[9px] md:text-[10px] text-slate-300 font-black uppercase tracking-[0.4em] pointer-events-none">Obsidian Protocol v44.0 Whisper & Wit</p>
+      <p className="text-center text-[9px] md:text-[10px] text-slate-300 font-black uppercase tracking-[0.4em] pointer-events-none">Obsidian Protocol v45.0 Strategic Conversationalist</p>
     </div>
   );
 };
